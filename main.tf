e0b5ed1901fe9613e0e448db63dfa5faa0d78dc5
root <root@ip-10-0-102-132.ec2.internal>
2021-09-16 13:26:15 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,17 @@ module "webserver" {
     source = "./modules/webserver"
 }
 
+module  "webserver2" {
+    source = "./modules/webserver2"
+}
+
+
+
 /*
 module "s3atlantis" {
     source = "./modules/s3-mod"
 }
 */
+
 
 
